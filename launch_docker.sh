2c347004f1d67696876c4c62967043961b2060dd
franchesoni <marchesoniacland@gmail.com>
2023-03-06 13:09:58 +0100

    update dir location

--- launch_docker.sh
+++ launch_docker.sh
@@ -1,4 +1,4 @@
 # if local
-docker run --volume ./backend/out:/code/vol/out --volume ./backend/app/images:/code/vol/images -p 8000:80 iaseg-app-dev
+docker run --volume ./vol/out:/vol/out --volume ./vol/images:/vol/images -p 8000:80 iaseg-app-dev
 # # if deploy
 # docker run -p 8000:80 --pull always franchesoni/iaseg-app 
