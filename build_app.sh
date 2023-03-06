f0f03edd0e24642c61cf4a40f7f8fe5aed627974
franchesoni <marchesoniacland@gmail.com>
2023-03-06 11:44:41 +0100

    add local dev option

--- build_app.sh
+++ build_app.sh
@@ -4,7 +4,10 @@ npm run build
 rm -rf ../backend/dist
 mv dist/ ../backend/dist
 cd ../backend
-docker build -t iaseg-app .
-docker tag iaseg-app franchesoni/iaseg-app
-docker push franchesoni/iaseg-app
+# if local
+docker build -t iaseg-app-dev .
+# # if deploy
+# docker build -t iaseg-app .
+# docker tag iaseg-app franchesoni/iaseg-app
+# docker push franchesoni/iaseg-app
 
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,17 +1,61 @@
 import './style.css';
 import { Viewer } from './viewer';
 
-export const IPaddress = "138.231.63.90"
-export const port = "2332"
+// if local/dev
+export const IPaddress = "localhost"
+export const port = "8000"
+// // if deploy
+// export const IPaddress = "138.231.63.90"
+// export const port = "2332"
 
 
+
+const viewer = new Viewer('viewer1', 1);
 let img: HTMLImageElement = new Image();
 const timestamp = new Date().getTime();  // hack to always reload
 img.src = `http://${IPaddress}:${port}/img?timestamp=${timestamp}`;
 
-const viewer = new Viewer('viewer1', 1)
-// wait for viewer to be ready before loading image
-// await new Promise(r => setTimeout(r, 1000));
-console.log('reset')
-img.addEventListener("load", () => viewer.handleImage(img));
+
+img.addEventListener("load", () => {
+  console.log("reload image")
+  viewer.reset()
+  viewer.handleImage(img)
+}
+);
+
+
+
+// const fileInput = document.getElementById('fileInput') as HTMLInputElement;
+// fileInput.addEventListener('change', async () => {
+//   const files = fileInput.files
+//   if (files && files.length > 0) {
+//   const file = files[0];
+//   const reader = new FileReader();
+//   reader.readAsArrayBuffer(file);
+
+//   reader.onload = async () => {
+//     const imgArrayBuffer = reader.result as ArrayBuffer;
+//     console.log(imgArrayBuffer)
+//     console.log("uploading image")
+
+//     const response = await fetch(`http://${IPaddress}:${port}/upload`, {
+//       method: 'POST',
+
+//       headers: {
+//       //   'Content-Type': 'image/jpeg'
+//       //   'Content-Type': 'application/octet-stream'
+//       },
+//       body: imgArrayBuffer
+//     });
+//     if (response.ok) {
+//       const imgBlob = new Blob([imgArrayBuffer], {type: 'image/jpeg'});
+//       img.src = URL.createObjectURL(imgBlob);  // resets and handles image
+//     }
+//     else {
+//       console.log("error when uploading image")
+//     }
+//   }
+//   };
+// }
+// );
 
