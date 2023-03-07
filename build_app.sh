a8d91026d59205b00a729e9986b0088d53a34f96
franchesoni <marchesoniacland@gmail.com>
2023-03-07 03:03:12 +0100

    update deployment scripts

--- build_app.sh
+++ build_app.sh
@@ -4,10 +4,7 @@ npm run build
 rm -rf ../backend/dist
 mv dist/ ../backend/dist
 cd ../backend
-# if local
-docker build -t iaseg-app-dev .
-# # if deploy
-# docker build -t iaseg-app .
-# docker tag iaseg-app franchesoni/iaseg-app
-# docker push franchesoni/iaseg-app
+docker build -t iaseg-app .
+docker tag iaseg-app franchesoni/iaseg-app
+docker push franchesoni/iaseg-app
 
--- launch_docker.sh
+++ launch_docker.sh
@@ -1,4 +1,1 @@
-# if local
-docker run --volume ./vol/out:/vol/out --volume ./vol/images:/vol/images -p 8000:80 iaseg-app-dev
-# # if deploy
-# docker run -p 8000:80 --pull always franchesoni/iaseg-app 
+docker run --volume ./vol/out:/vol/out --volume ./vol/images:/vol/images -p 8000:80 --pull always franchesoni/iaseg-app 
