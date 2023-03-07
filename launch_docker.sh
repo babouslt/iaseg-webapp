46c632daff3f67215ad17a0332200d70ae41a6d5
franchesoni <marchesoniacland@gmail.com>
2023-03-07 16:16:25 +0100

    update launch with path

--- launch_docker.sh
+++ launch_docker.sh
@@ -1,1 +1,3 @@
-docker run --volume ./vol/out:/vol/out --volume ./vol/images:/vol/images -p 8000:80 --pull always franchesoni/iaseg-app 
+# data dir should contain ./vol/out and ./vol/images  
+DATA_DIR=${PWD}
+docker run --volume ${DATA_DIR}/vol/out:/vol/out --volume ${DATA_DIR}/vol/images:/vol/images -p 8000:80 --pull always franchesoni/iaseg-app 
