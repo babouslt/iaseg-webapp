8687d21bc63608c5c3417499911c1987e0848d6a
franchesoni <marchesoniacland@gmail.com>
2023-03-17 04:00:16 +0100

    add gpu

--- launch_docker.sh
+++ launch_docker.sh
@@ -1,3 +1,3 @@
 # data dir should contain ./vol/out and ./vol/images  
 DATA_DIR=${PWD}
-docker run --volume ${DATA_DIR}/vol/out:/vol/out --volume ${DATA_DIR}/vol/images:/vol/images -p 8000:80 --pull always franchesoni/iaseg-app 
+docker run --volume ${DATA_DIR}/vol/out:/vol/out --volume ${DATA_DIR}/vol/images:/vol/images --gpus all -p 8000:80 --pull always franchesoni/iaseg-app 
